ae07c1fcfd4168fd7425df5750428038bf6ceeb3
Shalu Kumari <shaluk@syncroutesoftware.com>
2020-06-03 00:33:42 +0530

    Updates

---
+++ precache-manifest.6a561f978ca1cd16bdaada4b31aab966.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3248054a0d6ffab1f87fead239238adc",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "9efc3e190e38efe03d0e",
+    "url": "/monster-rolodex/static/css/main.46ace985.chunk.css"
+  },
+  {
+    "revision": "2670c331e07fe9266473",
+    "url": "/monster-rolodex/static/js/2.7ae0775c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.7ae0775c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9efc3e190e38efe03d0e",
+    "url": "/monster-rolodex/static/js/main.b2ee232e.chunk.js"
+  },
+  {
+    "revision": "9b1677f8689d9e6aab97",
+    "url": "/monster-rolodex/static/js/runtime-main.33dcb934.js"
+  }
+]);
